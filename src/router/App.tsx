b583f2e5a350bb9ff9e67abd9eb3ef3518309e0c
software vala <hellohostingvala@gmail.com>
2026-03-06 00:45:00 +0530

    Add /ai-builder route to render AIBuilderPage component

---
+++ src/router/App.tsx
@@ -0,0 +1,7 @@
+import { AIBuilderPage } from './path/to/AIBuilderPage';
+
+<Router>
+  {/* Other routes */}
+  <Route path="/ai-builder" component={AIBuilderPage} />
+  {/* Other routes */}
+</Router>
